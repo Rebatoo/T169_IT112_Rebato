764835e40afc6a407581e73cfe2dadc0e92a2337
Rebato_IvanRic <115499461+Rebatoo@users.noreply.github.com>
2022-10-14 12:50:21 +0800

    Add files via upload

---
+++ Hello World.c
@@ -0,0 +1,9 @@
+/*SIR during the previous meeting, my PC's Wifi connection was disabled, therefore I am unable to upload the file I created..*/
+#include <stdio.h>
+
+int main()
+{
+    printf("Ivan_Ric_Rebato");
+
+    return 0;
+}
